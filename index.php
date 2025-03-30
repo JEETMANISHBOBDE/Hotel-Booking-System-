eb214cf1250b3222315f86bb56fce6087b15bf7d
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-03-30 11:31:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -45,3 +45,22 @@
       </div>
     </div>
   </div>
+  <!-- check availability form -->
+
+  <div class="container availability-form">
+    <div class="row">
+      <div class="col-lg-12 bg-white shadow p-4 rounded">
+        <h5 class="mb-4">Check Booking Availability</h5>
+        <form action="rooms.php">
+          <div class="row align-items-end">
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Check-in</label>
+              <input type="date" class="form-control shadow-none" name="checkin" required>
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Check-out</label>
+              <input type="date" class="form-control shadow-none" name="checkout" required>
+            </div>
+            <div class="col-lg-3 mb-3">
+              <label class="form-label" style="font-weight: 500;">Adult</label>
+              <select class="form-select shadow-none" name="adult">
