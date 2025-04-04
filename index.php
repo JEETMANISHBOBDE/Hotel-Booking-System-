af3e43673ef661e7929668087ab770bf07f02fd1
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-04 11:02:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -203,3 +203,15 @@
                       $room_data[children] Children
                     </span>
                   </div>
+            $rating_data
+                  <div class="d-flex justify-content-evenly mb-2">
+                    $book_btn
+                    <a href="room_details.php?id=$room_data[id]" class="btn btn-sm btn-outline-dark shadow-none">More details</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          data;
+
+        }
+            
