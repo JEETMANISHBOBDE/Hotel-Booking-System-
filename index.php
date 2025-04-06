1909f4eed4c1022c7c1ff07021338de28622044f
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-06 11:15:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -230,3 +230,7 @@
       <?php 
         $res = mysqli_query($con,"SELECT * FROM `facilities` ORDER BY `id` DESC LIMIT 5");
         $path = FACILITIES_IMG_PATH;
+while($row = mysqli_fetch_assoc($res)){
+          echo<<<data
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+              <img src="$path$row[icon]" width="60px">
