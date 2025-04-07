5a9efa17ee3c081bc5d1c5ce7c88b3ee54c6b92e
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-07 12:17:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -239,3 +239,8 @@ while($row = mysqli_fetch_assoc($res)){
           data;
         }
       ?>
+ <div class="col-lg-12 text-center mt-5">
+        <a href="facilities.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+      </div>
+    </div>
+  </div>
