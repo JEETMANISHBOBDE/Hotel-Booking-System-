9ee37f259267dc344490a46f0fa04fe6e98b1bd6
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-11 17:14:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -227,3 +227,286 @@
       </div>
     </div>
   </div>
+
+  <!-- Our Facilities -->
+
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+      <?php 
+        $res = mysqli_query($con,"SELECT * FROM `facilities` ORDER BY `id` DESC LIMIT 5");
+        $path = FACILITIES_IMG_PATH;
+
+        while($row = mysqli_fetch_assoc($res)){
+          echo<<<data
+            <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+              <img src="$path$row[icon]" width="60px">
+              <h5 class="mt-3">$row[name]</h5>
+            </div>
+          data;
+        }
+      ?>
+
+      <div class="col-lg-12 text-center mt-5">
+        <a href="facilities.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+      </div>
+    </div>
+  </div>
+
+  <!-- Testimonials -->
+
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+
+  <div class="container mt-5">
+    <div class="swiper swiper-testimonials">
+      <div class="swiper-wrapper mb-5">
+        <?php
+
+          $review_q = "SELECT rr.*,uc.name AS uname, uc.profile, r.name AS rname FROM `rating_review` rr
+            INNER JOIN `user_cred` uc ON rr.user_id = uc.id
+            INNER JOIN `rooms` r ON rr.room_id = r.id
+            ORDER BY `sr_no` DESC LIMIT 6";
+
+          $review_res = mysqli_query($con,$review_q);
+          $img_path = USERS_IMG_PATH;
+
+          if(mysqli_num_rows($review_res)==0){
+            echo 'No reviews yet!';
+          }
+          else
+          {
+            while($row = mysqli_fetch_assoc($review_res))
+            {
+              $stars = "<i class='bi bi-star-fill text-warning'></i> ";
+              for($i=1; $i<$row['rating']; $i++){
+                $stars .= " <i class='bi bi-star-fill text-warning'></i>";
+              }
+
+              echo<<<slides
+                <div class="swiper-slide bg-white p-4">
+                  <div class="profile d-flex align-items-center mb-3">
+                    <img src="$img_path$row[profile]" class="rounded-circle" loading="lazy" width="30px">
+                    <h6 class="m-0 ms-2">$row[uname]</h6>
+                  </div>
+                  <p>
+                    $row[review]
+                  </p>
+                  <div class="rating">
+                    $stars
+                  </div>
+                </div>
+              slides;
+            }
+          }
+        
+        ?>
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+    <div class="col-lg-12 text-center mt-5">
+      <a href="about.php" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">Know More >>></a>
+    </div>
+  </div>
+
+  <!-- Reach us -->
+
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+        <iframe class="w-100 rounded" height="320px" src="<?php echo $contact_r['iframe'] ?>" loading="lazy"></iframe>
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Call us</h5>
+          <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+            <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
+          </a>
+          <br>
+          <?php 
+            if($contact_r['pn2']!=''){
+              echo<<<data
+                <a href="tel: +$contact_r[pn2]" class="d-inline-block text-decoration-none text-dark">
+                  <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                </a>
+              data;
+            }
+          
+          ?>
+        </div>
+        <div class="bg-white p-4 rounded mb-4">
+          <h5>Follow us</h5>
+          <?php 
+            if($contact_r['tw']!=''){
+              echo<<<data
+                <a href="$contact_r[tw]" class="d-inline-block mb-3">
+                  <span class="badge bg-light text-dark fs-6 p-2"> 
+                  <i class="bi bi-twitter me-1"></i> Twitter
+                  </span>
+                </a>
+                <br>
+              data;
+            }
+          ?>
+
+          <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block mb-3">
+            <span class="badge bg-light text-dark fs-6 p-2"> 
+            <i class="bi bi-facebook me-1"></i> Facebook
+            </span>
+          </a>
+          <br>
+          <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block">
+            <span class="badge bg-light text-dark fs-6 p-2"> 
+            <i class="bi bi-instagram me-1"></i> Instagram
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Password reset modal and code -->
+
+  <div class="modal fade" id="recoveryModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <form id="recovery-form">
+          <div class="modal-header">
+            <h5 class="modal-title d-flex align-items-center">
+              <i class="bi bi-shield-lock fs-3 me-2"></i> Set up New Password
+            </h5>
+          </div>
+          <div class="modal-body">
+            <div class="mb-4">
+              <label class="form-label">New Password</label>
+              <input type="password" name="pass" required class="form-control shadow-none">
+              <input type="hidden" name="email">
+              <input type="hidden" name="token">
+            </div>
+            <div class="mb-2 text-end">
+              <button type="button" class="btn shadow-none me-2" data-bs-dismiss="modal">CANCEL</button>
+              <button type="submit" class="btn btn-dark shadow-none">SUBMIT</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <?php require('inc/footer.php'); ?>
+
+  <?php
+    if(isset($_GET['account_recovery']))
+    {
+      $data = filteration($_GET);
+
+      $t_date = date("Y-m-d");
+
+      $query = select("SELECT * FROM `user_cred` WHERE `email`=? AND `token`=? AND `t_expire`=? LIMIT 1",
+        [$data['email'],$data['token'],$t_date],'sss');
+
+      if(mysqli_num_rows($query)==1)
+      {
+        echo<<<showModal
+          <script>
+            var myModal = document.getElementById('recoveryModal');
+
+            myModal.querySelector("input[name='email']").value = '$data[email]';
+            myModal.querySelector("input[name='token']").value = '$data[token]';
+
+            var modal = bootstrap.Modal.getOrCreateInstance(myModal);
+            modal.show();
+          </script>
+        showModal;
+      }
+      else{
+        alert("error","Invalid or Expired Link !");
+      }
+
+    }
+  ?>
+  
+  <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+  <script>
+    var swiper = new Swiper(".swiper-container", {
+      spaceBetween: 30,
+      effect: "fade",
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      }
+    });
+
+    var swiperTestimonials = new Swiper(".swiper-testimonials", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }
+    });
+
+    // recover account
+    
+    let recovery_form = document.getElementById('recovery-form');
+
+    recovery_form.addEventListener('submit', (e)=>{
+      e.preventDefault();
+
+      let data = new FormData();
+
+      data.append('email',recovery_form.elements['email'].value);
+      data.append('token',recovery_form.elements['token'].value);
+      data.append('pass',recovery_form.elements['pass'].value);
+      data.append('recover_user','');
+
+      var myModal = document.getElementById('recoveryModal');
+      var modal = bootstrap.Modal.getInstance(myModal);
+      modal.hide();
+
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST","ajax/login_register.php",true);
+
+      xhr.onload = function(){
+        if(this.responseText == 'failed'){
+          alert('error',"Account reset failed!");
+        }
+        else{
+          alert('success',"Account Reset Successful !");
+          recovery_form.reset();
+        }
+      }
+
+      xhr.send(data);
+    });
+
+  </script>
+
+</body>
+</html>
