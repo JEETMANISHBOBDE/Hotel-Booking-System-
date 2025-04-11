b69e941be6d5ee16e229231cb9477e96e8eec42c
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-11 17:12:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,3 +8,41 @@
   <?php require('inc/links.php'); ?>
   <title><?php echo $settings_r['site_title'] ?> - HOME</title>
   <style>
+    .availability-form{
+      margin-top: -50px;
+      z-index: 2;
+      position: relative;
+    }
+
+    @media screen and (max-width: 575px) {
+      .availability-form{
+        margin-top: 25px;
+        padding: 0 35px;
+      } 
+    }
+  </style>
+</head>
+<body class="bg-light">
+
+  <?php require('inc/header.php'); ?>
+
+  <!-- Carousel -->
+
+  <div class="container-fluid px-lg-4 mt-4">
+    <div class="swiper swiper-container">
+      <div class="swiper-wrapper">
+        <?php 
+          $res = selectAll('carousel');
+          while($row = mysqli_fetch_assoc($res))
+          {
+            $path = CAROUSEL_IMG_PATH;
+            echo <<<data
+              <div class="swiper-slide">
+                <img src="$path$row[image]" class="w-100 d-block">
+              </div>
+            data;
+          }
+        ?>
+      </div>
+    </div>
+  </div>
