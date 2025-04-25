655278482f8daec6b8461ec875b2802ca21638aa
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-04-25 21:45:54 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,20 +1,4 @@
-<?php
-include 'db.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
-
-    // Insert the user into the database
-    $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sss", $username, $email, $password);
-
-    if ($stmt->execute()) {
-        echo "<script>alert('Sign Up Successful!'); window.location.href = 'signin.php';</script>";
-    } else {
-        echo "<script>alert('Error: User already exists.');</script>";
+y exists.');</script>";
     }
 }
 ?>
