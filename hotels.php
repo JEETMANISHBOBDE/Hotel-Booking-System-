73a0697be5ecaec559d31707b8e38dadad59ec21
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-05-08 20:58:44 +0530

    Update hotels.php

--- hotels.php
+++ hotels.php
@@ -1,16 +1,1 @@
 
-    <div>
-        <?php if ($result->num_rows > 0): ?>
-            <?php while ($row = $result->fetch_assoc()): ?>
-                <div>
-                    <a href="view_rooms echo $row['hotel_id']; ?>">
-                        <?php echo $row['hotel_name']; ?>
-                    </a>
-                </div>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <p>No hotels available.</p>
-        <?php endif; ?>
-    </div>
-</body>
-</html>
