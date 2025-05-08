6e4450834fe2162dc61901b95864c15d92c4c15c
Jeet Bobde <147295141+JEETMANISHBOBDE@users.noreply.github.com>
2025-05-08 20:58:33 +0530

    Update hotels.php

--- hotels.php
+++ hotels.php
@@ -1,16 +1,4 @@
-<?php
-$sql = "SELECT * FROM hotels";
-$result = $conn->query($sql);
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Select Hotel</title>
-</head>
-<body>
     <div>
         <?php if ($result->num_rows > 0): ?>
             <?php while ($row = $result->fetch_assoc()): ?>
